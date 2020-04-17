dd64780e63c04fe6b75d44b6518cc4b9f045ccbe
Kiran Johns <kiranjohns369@gmail.com>
2020-04-17 20:16:56 +0530

    kyopp

--- notepadserver/settings.py
+++ notepadserver/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'd@kw2&8k6u2twq4fpacu1(jwjidcd44b)3*qxe$6sz+lc%1v%n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1','.herokuapp.com']
 
